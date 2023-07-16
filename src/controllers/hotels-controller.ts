2928375568d908ff8ebd64ed9c8db0eda496f310
Eduardo-JP-Ferreira <eduardopires2014@gmail.com>
2023-07-16 18:37:33 -0300

    Feat: Get Hotels - Errors Treatment

--- src/controllers/hotels-controller.ts
+++ src/controllers/hotels-controller.ts
@@ -6,7 +6,7 @@ import hotelsService from '@/services/hotels-service';
 export async function getHotels(req: AuthenticatedRequest, res: Response) {
   const { userId } = req;
   try {
-    const ticket = await hotelsService.getHotels(userId);
+    const ticket = await hotelsService.getHotels(userId, res);
     return res.status(httpStatus.OK).send(ticket);
   } catch (e) {
     return res.sendStatus(httpStatus.NOT_FOUND);
